65486ac3f3f2860efdcd7ca856b708198792853d
noah0920 <debilaurito@gmail.com>
2023-01-14 18:36:19 -0300

    empezando la galeria

---
+++ src/js/galeria.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function(){
+    iniciarApp();
+});
+
+function iniciarApp(){
+    crearGaleria();
+}
+
+function crearGaleria(){
+    const galeria = document.querySelector('.galeria-imagenes');
+
+    galeria.textContent = 'vamos a crear la galeria aca';
+}
