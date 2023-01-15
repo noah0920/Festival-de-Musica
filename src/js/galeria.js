7ecc830783e918dc3c3a55687a86e7f79ace59b7
noah0920 <debilaurito@gmail.com>
2023-01-14 23:56:22 -0300

    dsfsdf

--- src/js/galeria.js
+++ src/js/galeria.js
@@ -9,5 +9,45 @@ function iniciarApp(){
 function crearGaleria(){
     const galeria = document.querySelector('.galeria-imagenes');
 
-    galeria.textContent = 'vamos a crear la galeria aca';
+    for (let i = 1; i <= 12; i++) {
+        const imagen = document.createElement ('picture');
+        
+        imagen.innerHTML = `
+         
+        <img loading="lazy" width="200" height="300" src="img/thumb/${i}.jpg" alt="imagen galeria">
+        `;
+
+        imagen.onclick = function(){
+            mostrarImagen(i);
+        }
+        galeria.appendChild(imagen);
+        
+    }
+} 
+
+function mostrarImagen(id){
+    const imagen = document.createElement ('picture');
+        
+    imagen.innerHTML = `
+     
+    <img loading="lazy" width="200" height="300" src="img/grande/${id}.jpg" alt="imagen galeria">
+    `;
+    //crea el overlay con la imagen
+    const overlay = document.createElement ('DIV');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+
+    //boton para cerrar el modal
+    const cerrarModal = document.createElement('P');
+    cerrarModal.textContent = 'X';
+    cerrarModal.classList.add('.btn-cerrar');
+    overlay.appendChild(cerrarModal);
+
+    //lo añade al html
+    const body = document.querySelector ('body');
+    body.appendChild(overlay);
+
+
+
+
 }
